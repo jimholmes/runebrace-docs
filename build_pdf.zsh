28c44155e484eadd13438c04d07b053c7cfde8f5
Jim Holmes <Jim@GuidepostSystems.com>
2026-09-04 11:55:42 -0700

    First working version

---
+++ build_pdf.zsh
@@ -0,0 +1,10 @@
+pandoc Index.md \
+  guides/Overview.md \
+  guides/GettingStarted.md \
+  guides/SupportingModels.md \
+  guides/WorkingWithMeshes.md \
+  guides/Hollowing.md \
+  guides/PrintingAndSlicing.md \
+  guides/Glossary.md \
+-o output/RuneBraceManual.pdf \
+--resource-path=.:./assets/img:./guides
