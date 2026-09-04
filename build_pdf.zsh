d59a1ca31737b4bb298808b0c32107950e66f0b2
Jim Holmes <Jim@GuidepostSystems.com>
2026-09-04 14:23:06 -0700

    Fix for headers and floating images.

--- build_pdf.zsh
+++ build_pdf.zsh
@@ -6,5 +6,7 @@ pandoc Index.md \
   guides/Hollowing.md \
   guides/PrintingAndSlicing.md \
   guides/Glossary.md \
+-f markdown-implicit_figures \
+-H header.tex \
 -o output/RuneBraceManual.pdf \
 --resource-path=.:./assets/img:./guides
